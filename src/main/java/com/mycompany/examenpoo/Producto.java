a66bbc06fe22a19ca7aa25210a9410f70e82dd54
Jamir Patino <jamir2308@gmail.com>
2022-04-28 15:29:56 -0500

    first commit

---
+++ src/main/java/com/mycompany/examenpoo/Producto.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.examenpoo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto {
+    private String nombre;
+    private double valorUnidad;
+    private int cantidad;
+    private double total;
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getValorUnidad() {
+        return valorUnidad;
+    }
+
+    public void setValorUnidad(double valorUnidad) {
+        this.valorUnidad = valorUnidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
+    
+}
